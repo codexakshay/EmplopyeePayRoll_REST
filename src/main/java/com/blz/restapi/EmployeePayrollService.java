baccc9f2e34f2f96bab030cea64d152846ba1cd1
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-30 09:26:38 +0530

    Added : Ability to Update Employee Salary In JSON Server

---
+++ src/main/java/com/blz/restapi/EmployeePayrollService.java
@@ -0,0 +1,41 @@
+package com.blz.restapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayrollService {
+
+	public enum IOService {
+		DB_IO, FILE_IO, REST_IO
+	}
+
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = new ArrayList<>(employeePayrollList);
+	}
+
+	public EmployeePayrollService() {
+	}
+
+	public long countEntries(IOService ioService) {
+		return employeePayrollList.size();
+	}
+
+	public void addEmployeeToPayroll(EmployeePayrollData employeePayrollData, IOService ioService) {
+		employeePayrollList.add(employeePayrollData);
+	}
+
+	public void updateEmployeeSalary(String name, double salary, IOService ioService) {
+		EmployeePayrollData employeePayrollData = this.getEmployeePayrollData(name);
+		if (employeePayrollData != null)
+			employeePayrollData.salary = salary;
+	}
+
+	public EmployeePayrollData getEmployeePayrollData(String name) {
+		EmployeePayrollData employeePayrollData;
+		employeePayrollData = this.employeePayrollList.stream().filter(dataItem -> dataItem.name.equals(name))
+				.findFirst().orElse(null);
+		return employeePayrollData;
+	}
+}
