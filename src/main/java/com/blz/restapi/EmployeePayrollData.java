5c201aa710284f49deff871988a78ebb82ca3219
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-30 09:23:59 +0530

    Added : Ability Add New Employee To JSON Server

---
+++ src/main/java/com/blz/restapi/EmployeePayrollData.java
@@ -0,0 +1,20 @@
+package com.blz.restapi;
+
+import java.time.LocalDate;
+
+public class EmployeePayrollData {
+
+	public int id;
+	public String name;
+	public String gender;
+	public double salary;
+	public LocalDate startDate;
+
+	public EmployeePayrollData(int id, String name, String gender, double salary, LocalDate startDate) {
+		this.id = id;
+		this.name = name;
+		this.gender = gender;
+		this.salary = salary;
+		this.startDate = startDate;
+	}
+}
---
+++ src/test/java/com/blz/restapi/EmployeePayrollServiceTest.java
@@ -0,0 +1,71 @@
+package com.blz.restapi;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.blz.restapi.EmployeePayrollService.IOService;
+import com.google.gson.Gson;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class EmployeePayrollServiceTest {
+	
+	private static final IOService REST_IO = null;
+	static EmployeePayrollService employeePayrollRESTService;
+
+	@BeforeClass
+	public static void createObj() {
+		employeePayrollRESTService = new EmployeePayrollService();
+	}
+
+	@AfterClass
+	public static void nullObj() {
+		employeePayrollRESTService = null;
+	}
+
+	@Before
+	public void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = 3000;
+	}
+
+	private EmployeePayrollData[] getEmployeeList() {
+		Response response = RestAssured.get("/employeepayroll");
+		System.out.println("Employee Payroll entries in JsonServer :\n" + response.asString());
+		EmployeePayrollData[] arrayOfEmployee = new Gson().fromJson(response.asString(), EmployeePayrollData[].class);
+		return arrayOfEmployee;
+	}
+
+	private Response addEmployeeToJsonServer(EmployeePayrollData employeePayrollData) {
+		String employeeJson = new Gson().toJson(employeePayrollData);
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		request.body(employeeJson);
+		return request.post("/employeepayroll");
+	}
+
+	@Test
+	public void givenNewEmployee_WhenAdded_ShouldMatchResponseCode201AndCount() {
+		EmployeePayrollData[] arrayOfEmployee = getEmployeeList();
+		employeePayrollRESTService = new EmployeePayrollService(Arrays.asList(arrayOfEmployee));
+
+		EmployeePayrollData employeePayrollData = new EmployeePayrollData(0, "Mark", "M", 3000000.00, LocalDate.now());
+
+		Response response = addEmployeeToJsonServer(employeePayrollData);
+		int statusCode = response.getStatusCode();
+		Assert.assertEquals(201, statusCode);
+
+		employeePayrollData = new Gson().fromJson(response.asString(), EmployeePayrollData.class);
+		employeePayrollRESTService.addEmployeeToPayroll(employeePayrollData, REST_IO);
+		long entries = employeePayrollRESTService.countEntries(REST_IO);
+		Assert.assertEquals(3, entries);
+	}
+}
