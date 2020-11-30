f0bd210fe213de20eaaabcaa32e12fa957131651
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-11-30 09:25:32 +0530

    Added : Ability to Add Multiple Employee To Employee Payroll Using JSON Server

---
+++ src/main/java/com/blz/restapi/EmployeePayrollService.java
@@ -0,0 +1,28 @@
+package com.blz.restapi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeePayrollService {
+
+	public enum IOService {
+		DB_IO, FILE_IO, REST_IO
+	}
+
+	private List<EmployeePayrollData> employeePayrollList;
+
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = new ArrayList<>(employeePayrollList);
+	}
+
+	public EmployeePayrollService() {
+	}
+
+	public long countEntries(IOService ioService) {
+		return employeePayrollList.size();
+	}
+
+	public void addEmployeeToPayroll(EmployeePayrollData employeePayrollData, IOService ioService) {
+		employeePayrollList.add(employeePayrollData);
+	}
+}
